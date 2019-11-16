dcf05cc9384f1240b52076564b79c29f2dbeaabb
Dr B P Sharma <drbpsharma@outlook.com>
2019-11-16 20:25:15 +0530

    Add files via upload

---
+++ biggest3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a,b,c,g;
+	cout <<"Enter three numbers : ";
+	cin>>a>>b>>c;
+	
+	g=a>b && a>c ? a : b>c ? b : c;
+	
+	cout << "Greatest is " << g;
+}
+
---
+++ voter.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	cout << "Enter your name "; 
+	char name[51]; cin >> name;
+	cout << "Enter your age ";
+	int age; cin >> age;
+	if(age>=18)
+		cout <<"Dear " << name << " you can vote";
+	else
+		cout <<"Dear " << name << " you cannot vote";
+}
